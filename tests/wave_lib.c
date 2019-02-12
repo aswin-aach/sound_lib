ed20dd2be7926e574db5354759c4df6068154c5a
Balamuraleekrishna <kbalamuralee@gmail.com>
2019-02-13 00:43:58 +0530

    Removed sine wave creator.

--- tests/wave_lib.c
+++
@@ -1,8 +0,0 @@
-#include<math.h>
-
-void create_sine(int * wave, long s_rate, float freq, long n_s){
-    for(int i = 0; i < n_s; i++) {
-        *(wave + i) = sin(2 * M_PI * freq * (float) i / s_rate);
-        printf("%d", *(wave + i));
-    }
-}
--- tests/wave_lib.h
+++
@@ -1,6 +0,0 @@
-#ifndef create_sine_h
-#define create_sine_h
-
-void create_sine(int * wave, long s_rate, float freq, long n_s);
-
-#endif
